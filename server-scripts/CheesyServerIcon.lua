d25474ae3df863744400eadfd1f3ae54322cc64e
unflood123 <100457280+unflood123@users.noreply.github.com>
2022-12-25 04:04:19 -0500

    Update CheesyServerIcon.lua

--- server-scripts/CheesyServerIcon.lua
+++ server-scripts/CheesyServerIcon.lua
@@ -12,7 +12,7 @@ local image_0 = {
 
 --image_1 is used as the icon
 local image_1 = {
-    ['src'] = 'public-url-link-goes-here',
+    ['src'] = 'https://thumbs.dreamstime.com/b/funny-face-12963753.jpg',
     ['sizeX'] = 128,
     ['sizeY'] = 128,
     ['paddingX'] = 50, --use this to align it, currently 50 pixels from top right
